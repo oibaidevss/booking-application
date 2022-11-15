a1b97bf5371291a484e37787d376ebf2210b1203
sheanlouise.margallo <sheanlouise.margallo@fligno.com>
2022-11-15 14:11:40 +0800

    Number must be 11 digits

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,7 +38,7 @@ class RegisteredUserController extends Controller
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'number' => ['required', 'string', 'numeric', 'min:11', 'unique:users'],
+            'number' => ['required', 'string', 'numeric', 'digits:11', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
         
