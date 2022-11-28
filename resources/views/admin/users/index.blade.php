893a9ac6fdb7a5cfe0b640a77145a641ef255e9c
oibaidevss <sheanlouisemargallo@gmail.com>
2022-11-29 07:27:12 +0800

    Remove Column - Identification

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -31,12 +31,7 @@
                                     <th
                                         class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                         Type</th>
-                                    <th
-                                        class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-
-                                        Identification
-
-                                        </th>
+                                   
                                     <th
                                         class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
 
@@ -98,29 +93,7 @@
 
                                         </p>
                                     </td>
-                                    <td
-                                        class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                        <span
-                                            class="font-semibold leading-tight text-xs text-slate-400">
-                                            @if ($user->identification != '')
-                                                
-                                            <a target="_blank"  href="{{URL::to('/')}}/storage/identifications/{{$user->id}}/{{ $user->identification }}" target="_blank">
-                                                <button class="btn"><i class="fa fa-download"></i> Download File</button>
-                                            </a>
-                                            @endif
-
-                                            @if ($user->business_type === 'hotel')
-                                               
-                                                
-                                                @if($business->business_permit != '')
-                                                    <a target="_blank" href="{{URL::to('/')}}/storage/permits/hotel/{{$business->id}}/{{ $business->business_permit }}" target="_blank">
-                                                        <button class="btn"><i class="fa fa-download"></i> Download File</button>
-                                                    </a>
-                                                @endif
-
-                                            @endif
-                                        </span>
-                                    </td>
+                                    
                                     <td
                                         class="p-2 align-middle bg-transparent text-center border-b whitespace-nowrap shadow-transparent">
                                         <div class="align-center flex items-center ">
